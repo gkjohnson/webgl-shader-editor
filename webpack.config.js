e06d0240e44ced2ede8ed80a23eb419d6ddcf4ef
Garrett Johnson <garrett.kjohnson@gmail.com>
2017-09-19 15:21:53 -0700

    Add a static build

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: __dirname + '/index.html',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'index.bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [{ loader: 'wc-loader' }]
+            },
+            {
+                test: /\.js$/,
+                use: [{ loader: 'script-loader' }]
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './empty-template.ejs',
+            filename: 'index.bundle.html'
+        })
+    ]
+}
