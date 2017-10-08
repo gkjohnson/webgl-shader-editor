33d763dc4c53191398dc4b29a98624335b24e7c9
Garrett Johnson <garrett.kjohnson@gmail.com>
2017-10-08 10:40:01 -0700

    Remove menu

--- electron/app.js
+++ electron/app.js
@@ -1,10 +1,15 @@
-const electron = require('electron')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+const {
+    app,
+    BrowserWindow,
+    Menu
+} = require('electron')
 
 app.on('ready', () => {
     const main = new BrowserWindow()
     main.loadURL(`${__dirname}/../index.html`)
+    main.setMenu(null)
 })
 
 app.on('window-all-closed', () => app.quit())
+
+Menu.setApplicationMenu(new Menu())
